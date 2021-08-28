2a428e8d67432d16ace98bfb4276f6a5efa1fca1
Gunter <xc200925@gmail.com>
2021-08-28 14:19:56 +0900

    tapAddButton 함수 안에서  TextField 가 없는 Alert 이 표시되게 구현

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -17,6 +17,14 @@ class ViewController: UIViewController {
   }
   
   @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+    let alert = UIAlertController(title: "할 일 등록", message: "할 일을 입력해주세요.", preferredStyle: .alert)
+    let registerButton = UIAlertAction(title: "등록", style: .default, handler: { _ in
+
+    })
+    let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+    alert.addAction(cancelButton)
+    alert.addAction(registerButton)
+    self.present(alert, animated: true, completion: nil)
   }
 }
 
