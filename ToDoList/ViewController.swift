b28888f207070503124134405131ec4bf537131f
Gunter <xc200925@gmail.com>
2021-08-28 14:21:02 +0900

    tapAddButton 함수 안에서 Alert 에 TextFleid 가 표시되게 구현하고 등록 버튼을 누르면 textfield 에 작성한 text 를 가져옴

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -17,11 +17,14 @@ class ViewController: UIViewController {
   }
   
   @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
-    let alert = UIAlertController(title: "할 일 등록", message: "할 일을 입력해주세요.", preferredStyle: .alert)
+    let alert = UIAlertController(title: "할 일 등록", message: nil, preferredStyle: .alert)
     let registerButton = UIAlertAction(title: "등록", style: .default, handler: { _ in
-
+      debugPrint("\(alert.textFields?[0].text)")
     })
     let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+    alert.addTextField(configurationHandler: { textField in
+      textField.placeholder = "할 일을 입력해주세요."
+    })
     alert.addAction(cancelButton)
     alert.addAction(registerButton)
     self.present(alert, animated: true, completion: nil)
