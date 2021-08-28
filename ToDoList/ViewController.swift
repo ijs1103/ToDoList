ba301190ae582300b8165d343fc978bd3b5c641e
Gunter <xc200925@gmail.com>
2021-08-28 14:07:05 +0900

    아울렛 변수, 액션 함수 추가

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -8,8 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var tableView: UITableView!
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+
+  @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+  }
+  
+  @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
   }
 }
 
